6e37d9445d00d296eb39afa20b2b73f7ec5d54f7
mapankov <mapankov@users.noreply.github.com>
2015-04-06 14:26:02 +0600

    Create UnitTest.py

---
+++ 3003/UnitTest.py
@@ -0,0 +1,18 @@
+__author__ = 'pma'
+
+from unittest import TestCase
+from Lion import Lion
+import unittest
+import States
+
+
+class NumbersTest(TestCase):
+    def test_init(self):
+        lion = Lion(States.subjects[0], States.states[0], States.states[1], States.actions[0])
+        self.assertEqual(States.subjects[0], lion.Subject, 'Subject have wrong value')
+        self.assertEqual(States.states[0], lion.CurrentState, 'CurrentState have wrong value')
+        self.assertEqual(States.states[1], lion.FollowingState, 'FollowingState have wrong value')
+        self.assertEqual(States.actions[0], lion.FollowingAction, 'FollowingAction have wrong value')
+
+if __name__ == '__main__':
+    unittest.main()
