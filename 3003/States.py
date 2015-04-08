aad52a7ce5fc9ad83fc1e572064cd1e101b2c0ea
mapankov <mapankov@users.noreply.github.com>
2015-04-08 20:22:03 +0600

    Update States.py

--- 3003/States.py
+++ 3003/States.py
@@ -1,6 +1,6 @@
 __author__ = 'pma'
 
-from Lion import Lion
+from Transition import Transition
 
 # входные символы
 subjects = ['антилопа', 'охотник', 'дерево']
@@ -10,10 +10,10 @@ states = ['голодный', 'сытый']
 actions = ['съесть', 'спать', 'убежать', 'смотреть']
 
 # таблица переходов
-transitions = []
-transitions.append(Lion(subjects[0], states[0], states[1], actions[0]))
-transitions.append(Lion(subjects[1], states[0], states[0], actions[2]))
-transitions.append(Lion(subjects[2], states[0], states[0], actions[1]))
-transitions.append(Lion(subjects[0], states[1], states[0], actions[1]))
-transitions.append(Lion(subjects[1], states[1], states[0], actions[2]))
-transitions.append(Lion(subjects[2], states[1], states[0], actions[3]))
+transitions = {states[0]: {subjects[0]: Transition(states[1], actions[0]),
+                           subjects[1]: Transition(states[0], actions[2]),
+                           subjects[2]: Transition(states[0], actions[1])},
+               states[1]: {subjects[0]: Transition(states[0], actions[1]),
+                           subjects[1]: Transition(states[0], actions[2]),
+                           subjects[2]: Transition(states[0], actions[3])}
+               }
