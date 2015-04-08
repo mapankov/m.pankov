ecb41afb1cfb7a797cc18489f9cca3394d3ea831
mapankov <mapankov@users.noreply.github.com>
2015-04-08 20:21:14 +0600

    Update Main.py

--- 3003/Main.py
+++ 3003/Main.py
@@ -1,41 +1,39 @@
 __author__ = 'pma'
 
+
+from Lion import Lion
 import States
 
-# сначала лев голодный
-LionCurrent = States.states[0]
-# что лев увидел
-LionSaw = None
-# действие, которое совершит лев
-LionDoes = None
+# инициализируем льва
+lion = Lion(States.states[0], States.transitions)
 
 while True:
-    print('Состояние льва: ', LionCurrent)
+    # выводим текущее состояние льва
+    print('Состояние льва: ', lion.CurrentState)
     print()
     i = 0
+    out = ''
     # выводим список того, что он может увидеть
     for subject in States.subjects:
-        print(i, subject)
+        out += '[{0} {1}]'.format(i, subject)
         i += 1
-    print('* выход из программы')
+    out += '[* выход из программы]'
+    print(out)
     print()
     lock = True
     while lock:
-        act = input('Выберите, что увидел лев: ')
+        subj = input('Выберите, что увидел лев: ')
         try:
-            index = int(act)
+            index = int(subj)
             if 0 <= index <= len(States.subjects):
                 lock = False
         except ValueError:
-            if act == '*':
+            if subj == '*':
                 quit()
-    LionSaw = States.subjects[index]
     print()
-    print('Состояние льва: ', LionCurrent)
-    print('Лев увидел: ', LionSaw)
-    for lion in States.transitions:
-        if lion.Subject == LionSaw and lion.CurrentState == LionCurrent:
-            LionDoes = lion.FollowingAction
-            LionCurrent = lion.FollowingState
-            print('Действие льва: ', lion.FollowingAction)
-            break
+    # выводим предыдущее состояние льва
+    print('Состояние льва: ', lion.CurrentState)
+    # меняем его состояние
+    lion.setstate(States.subjects[index])
+    print('Лев увидел: ', lion.CurrentSubject)
+    print('Действие льва: ', lion.CurrentAction)
