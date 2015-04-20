fcd9bc35abd34775a4dbfaa8b45188c922e1d91f
mapankov <mapankov@users.noreply.github.com>
2015-04-20 14:45:57 +0600

    Create Transition.py

---
+++ 0604plus/Transition.py
@@ -0,0 +1,8 @@
+__author__ = 'pma'
+
+
+# вспомогательный класс для повышения читабельности кода
+class Transition:
+    def __init__(self, followingState, followingAction):
+        self.FollowingState = followingState
+        self.FollowingAction = followingAction
